d481414847a5d23ad6e76d895ab1272996648cb5
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-20 20:49:29 -0300

    update program

--- CleanArchitecture/API/Program.cs
+++ CleanArchitecture/API/Program.cs
@@ -1,10 +1,17 @@
 using Infra.Services;
 using Core.Services;
+using Infra.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigurePersistenceApp(builder.Configuration);
 builder.Services.ConfigureApplicationApp();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
