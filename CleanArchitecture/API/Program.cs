9f7c9155a3d9cfaa26df8b3ca9265c18806a2891
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-01-20 09:02:38 -0300

    refactor: program

--- CleanArchitecture/API/Program.cs
+++ CleanArchitecture/API/Program.cs
@@ -1,15 +1,18 @@
+using Infra.Services;
+using Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.ConfigurePersistenceApp(builder.Configuration);
+builder.Services.ConfigureApplicationApp();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
